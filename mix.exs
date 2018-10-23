b25041403e42b95215d929bfb256a774f525e72c
Bruno Ribeiro <brunz36@gmail.com>
2018-10-23 01:18:09 -0400

    Update to version 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Identicon.MixProject do
   def project do
     [
       app: :identicon,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
